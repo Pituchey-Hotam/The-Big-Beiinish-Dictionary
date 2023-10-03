d949310d587c6d61f09a6770f80ebd300575d786
Yehuda Eisenberg <32451776+YehudaEi@users.noreply.github.com>
2023-10-03 23:34:48 +0300

    Add silence updates

--- TBBD/main.cpp
+++ TBBD/main.cpp
@@ -11,6 +11,7 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
 	tbbd_status_t status = TBBD_STATUS_UNINITIALIZED;
+	BOOL silence = TRUE;
 	WCHAR ErrorMessage[50] = { 0 };
 	TBBD& tbbd = TBBD::getInstance();
 
@@ -19,20 +20,32 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		goto l_cleanup;
 	}
 
-	status = tbbd.run(hInstance, nCmdShow);
-	if (TBBD_STATUS_SUCCESS != status) {
-		goto l_cleanup;
+	if (strstr(lpCmdLine, "--chack-for-updates") != nullptr)
+	{
+		status = tbbd.checkForUpdates();
+		if (TBBD_STATUS_SUCCESS != status) {
+			goto l_cleanup;
+		}
+	}
+	else {
+		silence = FALSE;
+		status = tbbd.run(hInstance, nCmdShow);
+		if (TBBD_STATUS_SUCCESS != status) {
+			goto l_cleanup;
+		}
 	}
 
 	status = TBBD_STATUS_SUCCESS;
 l_cleanup:
 	if (TBBD_STATUS_SUCCESS != status) {
-		if (TBBD_STATUS_WAMIN_WINHTTPSENDREQUEST_FAILED == status) {
-			MessageBoxW(NULL, L"תוכנת מילון הבייניש הגדול דורשת חיבור לאינטרנט כדי לפעול", L"שגיאה!", MB_OK | MB_ICONERROR | MB_RTLREADING);
-		}
-		else {
-			wsprintfW(ErrorMessage, L"מספר שגיאה: %d\nאנא פנה לתמיכה.", (int)status);
-			MessageBoxW(NULL, ErrorMessage, L"שגיאה!", MB_OK | MB_ICONERROR | MB_RTLREADING);
+		if (!silence) {
+			if (TBBD_STATUS_WAMIN_WINHTTPSENDREQUEST_FAILED == status) {
+				MessageBoxW(NULL, L"תוכנת מילון הבייניש הגדול דורשת חיבור לאינטרנט כדי לפעול", L"שגיאה!", MB_OK | MB_ICONERROR | MB_RTLREADING);
+			}
+			else {
+				wsprintfW(ErrorMessage, L"מספר שגיאה: %d\nאנא פנה לתמיכה.", (int)status);
+				MessageBoxW(NULL, ErrorMessage, L"שגיאה!", MB_OK | MB_ICONERROR | MB_RTLREADING);
+			}
 		}
 	}
 
